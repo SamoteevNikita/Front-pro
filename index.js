fffd3948d3f6a731d5b8fd3f2b6cc8f167948e71
SamoteevNikita <samoteev.nv@gmail.com>
2025-01-17 20:00:22 +0200

    add HW2

---
+++ index.js
@@ -0,0 +1,7 @@
+alert ('привет')
+alert ('давай посчитаем сколько секунд в одном часу');
+let number = prompt('напиши сколько часов ты хочешь посчитать, например 1')
+
+let secondsInHour = 3600
+let result = number * secondsInHour;
+alert(result);
