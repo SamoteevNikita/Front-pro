21e3698c4c40ccbce290fa8a228d8de9db561a23
SamoteevNikita <samoteev.nv@gmail.com>
2025-03-17 19:14:01 +0200

    add

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-let ladder = {
-  step: 0,
-  up: function () {
-      this.step++;
-      return this;
-  },
-  down: function () {
-      this.step--;
-      return this;
-  },
-  showStep: function () { // показує теперішню сходинку
-      alert(this.step);
-      return this;
-  }
-};
-ladder.up().down().up().up().up().up().down().down().showStep(); 
+let shop = {
+  "Елекстроника": [
+    {name: "Iphone16", price: 1200 },
+    {name: "Iphone13", price: 800 },
+    {name: "Iphone10", price: 500 }
+  ],
+  "Машины": [
+    {name: "BMW", price: 10000 },
+    {name: "Matiz", price: 2000 },
+    {name: "Toyota", price: 4000 }
+  ]
+}
+
+console.log("категории:")
+let categories = Object.keys(shop)
+categories.forEach((category, index) => console.log(`${index+1}. ${category}`))
+
+let categoryIndex = parseInt(prompt(`выберите категорию (1 или 2)`)) -1
+let selectredCategory = categories[categoryIndex]
+
+
+console.log(`товары в готегории ${selectredCategory}:`)
+shop[selectredCategory].forEach((product, index) => console.log(`${index +1}. ${product.name} - ${product.price} usd`))
+
+let productIndex = parseInt(prompt(`Выберите товар`)) -1;
+let selectredProduct = shop[selectredCategory][productIndex]
+
+let quantityOfProduct = parseInt(prompt(`Введите количество товара`))
+let totalPrice = selectredProduct.price * quantityOfProduct
+
+
+
+if (totalPrice => 4000) {
+  totalPrice = totalPrice * 0.8; 
+  alert(`Вы получили скидку в размере 20%, поздравляем! Сумма для оплаты: ${totalPrice} usd`);
+} else {
+  alert(`У вас нету скидок, сумма для оплаты: ${totalPrice} usd`);
+}
+
 
 
