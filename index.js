df574f7664e18ef8eaf288c52b066c534a20cfb5
SamoteevNikita <samoteev.nv@gmail.com>
2025-03-24 17:29:37 +0200

    addhw20

--- index.js
+++ index.js
@@ -1,44 +1,26 @@
+const body = document.querySelector("body");
+const addDivForBtn = document.createElement("div")
+body.appendChild(addDivForBtn)
+
+const containerForPer = document.createElement("div")
+containerForPer.style.display = "flex"
+body.appendChild(containerForPer)
+
+const emojis = ["😀", "😂", "🤓", "🙂", "😖"]
+
+emojis.map((emoji) => {
+    let counter = 0
+
+    const btn = document.createElement("button")
+    btn.textContent = emoji
+    addDivForBtn.appendChild(btn)
 
 
-let inputBtn = document.getElementById('myTextField')
-let divBox = document.getElementById('infoBox')
-
-inputBtn.addEventListener( 'focus', () => {
-    divBox.style.display = "block"
+    const paragraph = document.createElement("p")
+    containerForPer.appendChild(paragraph)
+    btn.addEventListener('click', () => {
+        counter ++
+        paragraph.textContent = counter
+    })
 })
 
-inputBtn.addEventListener('blur', () => {
-    divBox.style.display = "none"
-});
-
-let btnFirst = document.getElementById('firstBtn')
-let btnSecond = document.getElementById('secondBtn')
-let userLink = ''
-
-btnFirst.addEventListener('click', () => {
-    let inputLink = prompt("Введіть посилання:");
-    if (inputLink && !inputLink.startsWith("http://") && !inputLink.startsWith("https://")) {
-        inputLink = "https://" + inputLink
-    }
-    userLink = inputLink;
-})
-
-
-btnSecond.addEventListener('click', () => {
-    if (userLink) {
-        window.location.href = userLink; 
-    } else {
-        alert("Спочатку введіть посилання!");
-    }
-})
-    
-
-const images = [
-    "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg",
-    "6.jpg", "7.jpg", "8.jpg", "9.jpg"
-];
-
-const randomIndex = Math.floor(Maths.randomIndex() * images.length);
-const randomImg = 'images/' +images[randomIndex]
-
-document.getElementById("randomImg").src = randomImage;
