576563aeb9421d9a5b9bd344452f203f80252fc7
SamoteevNikita <samoteev.nv@gmail.com>
2025-04-12 13:35:41 +0300

    add slider

--- index.js
+++ index.js
@@ -1,55 +1,41 @@
-class Hamburger {
-    
-    constructor(size, stuffing) {
-      this.size = size;
-      this.stuffing = stuffing;
-      this.toppings = []; 
-    }
-  
-    
-    addTopping(topping) {
-      if (!this.toppings.includes(topping)) {
-        this.toppings.push(topping);
-      }
-    }
-  
-   
-    calculatePrice() {
-      let total = this.size.price + this.stuffing.price;
-      this.toppings.forEach(t => total += t.price);
-      return total;
-    }
-  
-    
-    calculateCalories() {
-      let total = this.size.calories + this.stuffing.calories;
-      this.toppings.forEach(t => total += t.calories);
-      return total;
-    }
-  
-    
-    static SIZE_SMALL = { price: 50, calories: 20 };
-    static SIZE_LARGE = { price: 100, calories: 40 };
-  
-    static STUFFING_CHEESE = { price: 10, calories: 20 };
-    static STUFFING_SALAD = { price: 20, calories: 5 };
-    static STUFFING_POTATO = { price: 15, calories: 10 };
-  
-    static TOPPING_SAUCE = { price: 15, calories: 0 };
-    static TOPPING_MAYO = { price: 20, calories: 5 };
+const images = [
+  'https://miro.medium.com/v2/resize:fit:400/1*0V8IZH_wHIo7yZvVYm5fVQ.jpeg',
+  'https://habrastorage.org/files/61d/d9e/d20/61dd9ed201254aeba4e52189db698b6a.jpg',
+  'https://monsterlessons.com/api/storage/uploads/posters/9d8f3bee-100d-49b5-9973-49e7b04077e0/poster.png',
+  'https://cdn.otus.ru/media/public/1b/b3/Node_Deep_24.1-5020-1bb356.png',
+];
+
+
+
+let currentIndex = 0;
+
+function showImage(index) {
+  let image = document.getElementById('image');
+  image.src = images[index];
+}
+
+document.querySelector('#nextbtn').addEventListener('click', () => {
+  currentIndex ++
+  if (currentIndex >= images.length) {
+    currentIndex = 0
   }
+  showImage(currentIndex)
+})
 
-  
-  
-const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
+document.querySelector('#prevbtn').addEventListener('click', () => {
+  currentIndex--
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1
+  }
+  showImage(currentIndex)
+})
 
+setInterval(() => {
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;  
+  }
+  showImage(currentIndex);
+}, 3000);  
 
-hamburger.addTopping(Hamburger.TOPPING_MAYO);
-
-console.log("Calories: " + hamburger.calculateCalories()); 
-console.log("Price: " + hamburger.calculatePrice());    
-
-  
-hamburger.addTopping(Hamburger.TOPPING_SAUCE);
-
-console.log("Price with sauce: " + hamburger.calculatePrice()); 
+showImage(currentIndex)
