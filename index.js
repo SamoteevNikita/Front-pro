174a0f8fb69a815a756c025c5760022ff584aa25
SamoteevNikita <samoteev.nv@gmail.com>
2025-04-14 15:32:51 +0300

    clock

--- index.js
+++ index.js
@@ -1,41 +1,29 @@
-const images = [
-  'https://miro.medium.com/v2/resize:fit:400/1*0V8IZH_wHIo7yZvVYm5fVQ.jpeg',
-  'https://habrastorage.org/files/61d/d9e/d20/61dd9ed201254aeba4e52189db698b6a.jpg',
-  'https://monsterlessons.com/api/storage/uploads/posters/9d8f3bee-100d-49b5-9973-49e7b04077e0/poster.png',
-  'https://cdn.otus.ru/media/public/1b/b3/Node_Deep_24.1-5020-1bb356.png',
-];
+
+function updateTime() {
+  const now = new Date();
+
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
 
 
+  const hourTens = Math.floor(hours / 10);
+  const hourOnes = hours % 10;
+  const minuteTens = Math.floor(minutes / 10);
+  const minuteOnes = minutes % 10;
+  const secondTens = Math.floor(seconds / 10);
+  const secondOnes = seconds % 10;
 
-let currentIndex = 0;
-
-function showImage(index) {
-  let image = document.getElementById('image');
-  image.src = images[index];
+  document.getElementById('hour-tens').textContent = hourTens;
+  document.getElementById('hour-ones').textContent = hourOnes;
+  document.getElementById('minute-tens').textContent = minuteTens;
+  document.getElementById('minute-ones').textContent = minuteOnes;
+  document.getElementById('second-tens').textContent = secondTens;
+  document.getElementById('second-ones').textContent = secondOnes;
 }
 
-document.querySelector('#nextbtn').addEventListener('click', () => {
-  currentIndex ++
-  if (currentIndex >= images.length) {
-    currentIndex = 0
-  }
-  showImage(currentIndex)
-})
 
-document.querySelector('#prevbtn').addEventListener('click', () => {
-  currentIndex--
-  if (currentIndex < 0) {
-    currentIndex = images.length - 1
-  }
-  showImage(currentIndex)
-})
+setInterval(updateTime, 1000);
 
-setInterval(() => {
-  currentIndex++;
-  if (currentIndex >= images.length) {
-    currentIndex = 0;  
-  }
-  showImage(currentIndex);
-}, 3000);  
 
-showImage(currentIndex)
+updateTime();
